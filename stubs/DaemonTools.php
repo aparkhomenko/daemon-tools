6a6525103bf8acf64c77e42b8adb0d9b5615a112
Alexey Parkhomenko <a.parkhomenko@iprojects.com.ua>
2015-02-10 22:41:35 +0200

    Add stub

---
+++ stubs/DaemonTools.php
@@ -0,0 +1,24 @@
+<?php 
+
+trait DaemonTools
+{
+    public function fdClose($fileDescriptor)
+    {
+        
+    }
+    
+    public function dup($oldHandle, $newHandle)
+    {
+        
+    }
+    
+    public function lock($resource)
+    {
+        
+    }
+    
+    public function unlock($resource)
+    {
+        
+    }
+}
